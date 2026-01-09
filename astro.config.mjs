3444264f55bf61d88022e3d712bfa8724b74cb8e
tobyzhong <zhongziyun1993@gmail.com>
2026-01-08 19:09:10 -0800

    Add phone and email contact details

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap()
+  ]
+});
